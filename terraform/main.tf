29382bfc26a6e5e06e070e20261a8c86f7b29e56
Weili Gao <weiligao@users.noreply.github.com>
2022-09-30 11:29:33 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,16 +6,16 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
