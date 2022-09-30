e6bce73c39f4736fdade00742f198b1bd229a780
Weili Gao <weiligao@users.noreply.github.com>
2022-09-30 11:22:06 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "wga-tfstate-rg"
+    storage_account_name = "wgatfstatesa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
